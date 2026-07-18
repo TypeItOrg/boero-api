b68f8c0e39d896a29d9f9cb4cf9f18654dd1f0fd
Matías Delgado <matiasdelgadodev@gmail.com>
2026-07-17 23:52:15 -0300

    refactor(auth): extend field conflict exception for existing users

--- src/main/java/ar/edu/utn/frvm/typeit/boero_api/auth/exceptions/UserAlreadyExistsException.java
+++ src/main/java/ar/edu/utn/frvm/typeit/boero_api/auth/exceptions/UserAlreadyExistsException.java
@@ -2,11 +2,10 @@ package ar.edu.utn.frvm.typeit.boero_api.auth.exceptions;
 
 import static ar.edu.utn.frvm.typeit.boero_api.auth.exceptions.AuthMessages.USER_ALREADY_EXISTS;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.server.ResponseStatusException;
+import ar.edu.utn.frvm.typeit.boero_api.common.exceptions.FieldConflictException;
 
-public class UserAlreadyExistsException extends ResponseStatusException {
+public class UserAlreadyExistsException extends FieldConflictException {
   public UserAlreadyExistsException() {
-    super(HttpStatus.CONFLICT, USER_ALREADY_EXISTS);
+    super("documentNumber", USER_ALREADY_EXISTS);
   }
 }
